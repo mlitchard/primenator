391c9b0fe5184ac67e1f5599d96830990bc97b3c
Michael Litchard <michael@schmong.org>
2016-12-01 19:16:04 +0000

    prime property test in place

---
+++ src-test/PrimeTest.hs
@@ -0,0 +1,22 @@
+module PrimeTest (propPrime) where
+
+import BasicPrelude
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Generator
+import Math.NumberTheory.Primes.Testing (isPrime)
+
+propPrime :: Int -> Spec
+propPrime ub = do
+  describe "QuickCheck testing fizzbuzz" $
+    modifyMaxSuccess (const maxS)        $
+    prop propMsg                         $
+    forAll (choose (10,ub)) verifyPrime
+  where
+    propMsg = "Lowerbound: 10 Upperbound: " <> showUB
+    showUB  = show ub
+    maxS    = (ub `div` 10) :: Int
+
+verifyPrime :: Int -> Bool
+verifyPrime p = all (== True) $ map isPrime $ take p primes 
